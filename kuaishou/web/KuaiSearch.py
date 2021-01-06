a8cd5eb391d745dbccb16a85959accd22ba2503d
wangkangxi <1548344039@qq.com>
2021-01-06 19:51:52 +0800

    简化相关参数

--- kuaishou/web/KuaiSearch.py
+++ kuaishou/web/KuaiSearch.py
@@ -13,13 +13,14 @@ headers = {
     "Origin": "https://video.kuaishou.com",
     # "Cookie": ""}
     # "Cookie": "did=web_a7914eac2bfa47efbbc1e885dce8c0c3; didv=1609816057000; kpf=PC_WEB; kpn=KUAISHOU_VISION; clientid=3; client_key=65890b29; Hm_lvt_86a27b7db2c5c0ae37fee4a8a35033ee=1609827084; userId=2195021671; ktrace-context=1|MS43NjQ1ODM2OTgyODY2OTgyLjcyODg3NDEzLjE2MDk4Mzg1ODEzODAuMjAyNQ==|MS43NjQ1ODM2OTgyODY2OTgyLjY1NzUxNDkxLjE2MDk4Mzg1ODEzODAuMjAyNg==|0|graphql-server|webservice|false|NA; kuaishou.server.web_st=ChZrdWFpc2hvdS5zZXJ2ZXIud2ViLnN0EqAB8vzPCgrjqAT7sRwu99fQkEHbqn6Tp8-UYrHo_yqm9mba3fqWwEuU8Udi27hSm3aFVJeMzkYpks1qgLF16BexhnkBwCspJysjbsry-ZunLrLcSO9fZgsIAfocInZ5oJQOYfa8v6I5vFGQIPgUHeR5NAK9guLlKyCVHjeDe70R_1GfKL7H2j5VecYAIeha8SljWkMxYNbDwzWGC5GW-u-_nhoSg3ZkWJHNsQvvc8vsvUksYr6BIiAhfXY70Edrf9hWvatCAhhuNuZB94r2SYds6q8W0xUxpygFMAE; kuaishou.server.web_ph=59943bd5bb9eeb348421e66eec939a144963"}
-    "Cookie": "did=web_a7914eac2bfa47efbbc1e885dce8c0c3; "
-              "didv=1609816057000; "
-              "kpf=PC_WEB; "
-              "kpn=KUAISHOU_VISION; "
-              "clientid=3; "
-              "client_key=65890b29; "
-              "Hm_lvt_86a27b7db2c5c0ae37fee4a8a35033ee=1609827084; "
+    "Cookie":
+              # "did=web_a7914eac2bfa47efbbc1e885dce8c0c3; "
+              # "didv=1609816057000; "
+              # "kpf=PC_WEB; "
+              # "kpn=KUAISHOU_VISION; "
+              # "clientid=3; "
+              # "client_key=65890b29; "
+              # "Hm_lvt_86a27b7db2c5c0ae37fee4a8a35033ee=1609827084; "
               "userId=2195021671; "
               "kuaishou.server.web_st=ChZrdWFpc2hvdS5zZXJ2ZXIud2ViLnN0EqAB8vzPCgrjqAT7sRwu99fQkEHbqn6Tp8-UYrHo_yqm9mba3fqWwEuU8Udi27hSm3aFVJeMzkYpks1qgLF16BexhnkBwCspJysjbsry-ZunLrLcSO9fZgsIAfocInZ5oJQOYfa8v6I5vFGQIPgUHeR5NAK9guLlKyCVHjeDe70R_1GfKL7H2j5VecYAIeha8SljWkMxYNbDwzWGC5GW-u-_nhoSg3ZkWJHNsQvvc8vsvUksYr6BIiAhfXY70Edrf9hWvatCAhhuNuZB94r2SYds6q8W0xUxpygFMAE"}
 
@@ -28,7 +29,7 @@ def get_video_list():
     json0 = {
         "operationName": "visionSearchPhoto",
         "variables": {
-            "keyword": "网易云",
+            "keyword": "天使熊",
             "pcursor": "1",
             "page": "searchPhoto",
             "searchSessionId": "MTRfMjE5NTAyMTY3MV8xNjA5ODQ4OTU1ODQ1X-e9keaYk-S6kV8yMzYx"
